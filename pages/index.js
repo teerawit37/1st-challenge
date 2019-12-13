473825caea7db25018a287f706380c826833b08f
Teerawit Kaewphirom <teerawit.kae@tourkrub.co>
2019-12-13 12:31:22 +0700

    add pagination

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import TablePagination from '@material-ui/core/TablePagination';
+import { DataTable } from 'react-data-components';
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {data: []}
+    this.state = {
+      data: [],
+      column: [
+        { title: 'id', prop: 'id'  },
+        { title: 'name', prop: 'name' },
+        { title: 'pulls_url', prop: 'pulls_url' },
+        { title: 'full_name', prop: 'full_name' }
+      ]
+    }
   }
   componentDidMount() {
     this.getList();
@@ -23,26 +24,20 @@ class Home extends Component {
     .then(response => {
      this.setState({
        data: response
-     },
-     console.log(response)
+     }
      )
     })
   }
   render(){
-  const { data } = this.state;
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
+  const { data, column } = this.state;
   return (
   <div>
     <Head>
       <title>Home</title>
       <link rel="icon" href="/favicon.ico" />
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"/>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"/>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/carlosrocha/react-data-components@master/css/table-twbs.css"/>
     </Head>
 
     <Nav />
@@ -52,68 +47,15 @@ class Home extends Component {
       <p className="description">
         To get started, edit <code>pages/index.js</code> and save to reload.
       </p>
-      {/* <div className="row">
-      <TableBody>
-          {data.map(item => (
-            <TableRow key={item.name}>
-              <TableCell component="th" scope="row">
-                {item.name}
-              </TableCell>
-              <TableCell align="right">{item.id}</TableCell>
-              <TableCell align="right">{item.name}</TableCell>
-              <TableCell align="right">{item.pulls_url}</TableCell>
-              <TableCell align="right">{item.full_name}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </div> */}
-      <Paper className={classes.root}>
-      <div className={classes.tableWrapper}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map(column => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map(column => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </div>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={data.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
+      <DataTable
+      keys="name"
+      columns={column}
+      initialData={data}
+      initialPageLength={10}
       />
-    </Paper>
+      </div>
  
-    </div>
-
+   
     <style jsx>{`
       .hero {
         width: 100%;
@@ -166,3 +108,5 @@ class Home extends Component {
 }
 
 export default Home
+
+
